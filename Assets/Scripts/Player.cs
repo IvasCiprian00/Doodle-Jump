998bdc43ba38dfa4469a58d1575499131cbcb348
Ciprian Ivas <ciprian@chirpy.digital>
2023-10-12 17:20:26 +0300

    Add movement and better jumping

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,16 +10,59 @@ public class Player : MonoBehaviour
     [SerializeField]
     private float jumpForce;
 
-    // Update is called once per frame
+    [SerializeField]
+    private float speed;
+
+    [SerializeField]
+    private BoxCollider2D boxCollider2D;
+
+    private bool isAscending = false;
     void Update()
     {
-        
+        Movement();
     }
+
     void OnCollisionEnter2D(Collision2D col)
     {
-        if(col.gameObject.name == "Platform")
+        if(col.gameObject.tag == "Platform")
         {
             rigidBody2D.AddForce(transform.up * jumpForce);
+            StartJump(true);
         }
+    }
+
+    void Movement()
+    {
+        transform.eulerAngles = new Vector3(0, 0, 0);
+
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            transform.Translate(-transform.right * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(transform.right * speed * Time.deltaTime);
+        }
+
+        if (isAscending)
+        {
+            boxCollider2D.enabled = false;
+        }
+        else
+        {
+            boxCollider2D.enabled = true;
+        }
+    }
+
+    void StartJump(bool condition)
+    {
+        isAscending = condition;
+        StartCoroutine(EndJump());
+    }
+
+    IEnumerator EndJump()
+    {
+        yield return new WaitForSeconds(0.5f);
+        isAscending = false;
     }
 }
