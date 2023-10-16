eea8942dc7ec10d7a8db8f2df486d9c84cc13668
Ciprian Ivas <ciprian@chirpy.digital>
2023-10-16 22:01:04 +0300

    Platforms disapear offscreen

---
+++ Assets/Scripts/PlatformScript.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformScript : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject background;
+
+    private void Start()
+    {
+        background = GameObject.Find("Background");
+    }
+    void Update()
+    {
+        if(background.transform.position.y - gameObject.transform.position.y > 5f)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
